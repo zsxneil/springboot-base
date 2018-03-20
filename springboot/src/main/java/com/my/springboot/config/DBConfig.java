c2545cafb073fc0e053362988c11103af32d3379
zsxneil <zsxneil@hotmail.com>
2018-03-20 20:55:07 +0800

    数据库信息改为从配置文件中读取

--- springboot/src/main/java/com/my/springboot/config/DBConfig.java
+++ springboot/src/main/java/com/my/springboot/config/DBConfig.java
@@ -2,6 +2,7 @@ package com.my.springboot.config;
 
 import com.alibaba.druid.pool.DruidDataSource;
 import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
@@ -14,14 +15,24 @@ import java.util.Properties;
 @MapperScan(basePackages = {"com.my.springboot.mybatis.mappers"})
 public class DBConfig {
     //我们只需要配置一个数据源即可，mybatis-spring-boot-starter会自动帮我们创建SqlSessionFactory和SqlSessionTemplate
+
+    @Value("${db.url}")
+    private String dbUrl;
+    @Value("${db.username}")
+    private String dbUserName;
+    @Value("${db.password}")
+    private String dbPassword;
+    @Value("${db.driverName}")
+    private String dbDriverName;
+
     @Primary
     @Bean
     public DataSource druidDataSource() {
         DruidDataSource dataSource = new DruidDataSource();
-        dataSource.setUrl("jdbc:mysql://localhost:3306/mybatis?useUnicode=true&characterEncoding=UTF-8&useSSL=true");
-        dataSource.setUsername("root");
-        dataSource.setPassword("123456");
-        dataSource.setDriverClassName("com.mysql.jdbc.Driver");
+        dataSource.setUrl(dbUrl);
+        dataSource.setUsername(dbUserName);
+        dataSource.setPassword(dbPassword);
+        dataSource.setDriverClassName(dbDriverName);
         return  dataSource;
 
     }
